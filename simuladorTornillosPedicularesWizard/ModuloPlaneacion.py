47822e4a98af18df9b5164a0d56220a451981b58
qcamiloq <qcamiloq@gmail.com>
2016-05-21 20:22:50 -0500

    Box de instrucciones listo para modificar en cada paso y guiar al usuario en la interaccion con el software

--- simuladorTornillosPedicularesWizard/ModuloPlaneacion.py
+++ simuladorTornillosPedicularesWizard/ModuloPlaneacion.py
@@ -12,7 +12,7 @@ class ModuloPlaneacion(ctk.ctkWorkflowWidgetStep) :
 
 		self.__layout = qt.QFormLayout( self )
     
-		font=qt.QFont("Sans Serif", 12, qt.QFont.Bold)
+		font=qt.QFont("Sans Serif",10, qt.QFont.Bold)
 
 #Definicion botenes colapsables:
 #--------------------------------------------------------------------------------------------------
@@ -27,7 +27,26 @@ class ModuloPlaneacion(ctk.ctkWorkflowWidgetStep) :
 		self.botonColapsableManipulacionTornillos = ctk.ctkCollapsibleButton()
 		self.botonColapsableManipulacionTornillos.text = "Manipulacion de tornillos"
 		self.__layout.addRow(self.botonColapsableManipulacionTornillos)
-   
+
+#Interfaz grafica de cada boton colapsable
+#--------------------------------------------------------------------------------------------------
+
+#Instrucciones de uso:
+		self.instruccionesDeUsoLayout=qt.QFormLayout(self.botonColapsableInstrucciones)
+		self.instruccionesdeUsoFrame=qt.QFrame()
+		self.instruccionesdeUsoFrame.setFrameShadow(1)
+		self.instruccionesdeUsoFrame.setGeometry(0,0,7,6)
+		self.instruccionesdeUsoFrameLayout=qt.QFormLayout()
+		self.labelInstruccionesDeUsoBienvenido = qt.QLabel("Bienvenido al modulo de planeacion")
+		self.labelInstruccionesDeUsoBienvenido.setFont(font)
+		self.labelInstruccionesDeUsoInstruccion = qt.QLabel("1. Ubique el primer punto de insercion")
+		self.labelInstruccionesDeUsoInstruccion.setFont(font)
+		self.instruccionesdeUsoFrameLayout.addWidget(self.labelInstruccionesDeUsoBienvenido)
+		self.instruccionesdeUsoFrameLayout.addWidget(self.labelInstruccionesDeUsoInstruccion)
+
+		self.instruccionesdeUsoFrame.setLayout(self.instruccionesdeUsoFrameLayout)  
+		self.instruccionesDeUsoLayout.addRow(self.instruccionesdeUsoFrame)
+
   def onEntry(self, comingFrom, transitionType):
     super(ModuloPlaneacion, self).onEntry(comingFrom, transitionType)
     self.ctimer = qt.QTimer()
