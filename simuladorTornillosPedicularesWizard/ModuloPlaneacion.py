c4bd007702e5785af3c7af069f8f82848adb6f6f
qcamiloq <qcamiloq@gmail.com>
2016-05-21 20:44:13 -0500

    ComboBox en manipulacion de tornillos que busca los modelos de tornillos disponibles para cargar funcional

--- simuladorTornillosPedicularesWizard/ModuloPlaneacion.py
+++ simuladorTornillosPedicularesWizard/ModuloPlaneacion.py
@@ -1,5 +1,7 @@
 # -*- coding: UTF-8 -*-
 from __main__ import vtk, qt, ctk, slicer
+from os import listdir
+from os.path import isfile, join
 
 class ModuloPlaneacion(ctk.ctkWorkflowWidgetStep) :
 
@@ -47,10 +49,23 @@ class ModuloPlaneacion(ctk.ctkWorkflowWidgetStep) :
 		self.instruccionesdeUsoFrame.setLayout(self.instruccionesdeUsoFrameLayout)  
 		self.instruccionesDeUsoLayout.addRow(self.instruccionesdeUsoFrame)
 
+#Ubicacion de los tornillos:
+		self.ubicacionDeTornillosLayout=qt.QFormLayout(self.botonColapsableUbicacionTornillos)
+		self.seleccionTornillo1ComboBox = qt.QComboBox()
+		self.pathTornillos="C:\Users\Camilo_Q\Documents\GitHub\simuladorTornillosPediculares\simuladorTornillosPedicularesWizard\Modelos\Tornillos"
+		self.onlyfiles = [f for f in listdir(self.pathTornillos) if isfile(join(self.pathTornillos, f))] #Lista los archivos que estan dentro del path
+		for tornillo in self.onlyfiles: #Muestra en el comboBox de cursos los archivos que estan presentes en el path
+			self.seleccionTornillo1ComboBox.addItem(tornillo)
+		self.ubicacionDeTornillosLayout.addRow(self.seleccionTornillo1ComboBox)
+
+
+
   def onEntry(self, comingFrom, transitionType):
     super(ModuloPlaneacion, self).onEntry(comingFrom, transitionType)
     self.ctimer = qt.QTimer()
     self.ctimer.singleShot(0, self.killButton)
+    self.cargarScene()
+
 
   def onExit(self, goingTo, transitionType):
     super(ModuloPlaneacion, self).onExit(goingTo, transitionType)
@@ -62,4 +77,28 @@ class ModuloPlaneacion(ctk.ctkWorkflowWidgetStep) :
   def killButton(self):
     bl = slicer.util.findChildren(text='ModuloPlaneacion' )
     bl[0].hide()
+
+  def cargarScene(self):
+    path1='C:\Users\Camilo_Q\Documents\GitHub\simuladorTornillosPediculares\simuladorTornillosPedicularesWizard\Modelos/stlcolumna.stl' #Se obtiene direccion de la unbicación del tornillo
+    path2='C:\Users\Camilo_Q\Documents\GitHub\simuladorTornillosPediculares\simuladorTornillosPedicularesWizard\Modelos\Lumbar 2.5 B31s - 4/4 Lumbar  2.5  B31s.nrrd'
+    slicer.util.loadModel(path1)
+    slicer.util.loadVolume(path2)
+
+    columna=slicer.util.getNode('stlcolumna') # Se obtiene el nodo del objeto en escena
+    columnaModelDisplayNode = columna.GetDisplayNode() 
+    columnaModelDisplayNode.SetColor(0.9451,0.8392,0.5686) #Colores parametrizados sobre 255
+    columnaModelDisplayNode.SetSliceIntersectionVisibility(1)
+    layoutManager = slicer.app.layoutManager() 
+    threeDWidget = layoutManager.threeDWidget(0)
+    self.threeDView = threeDWidget.threeDView()
+    self.threeDView.resetFocalPoint()
+#Ubiar la camara en la parte posterior
+    cameraNode=slicer.util.getNode('*Camera*') 
+    camera=cameraNode.GetCamera() 
+    camera.SetPosition(-5.92673, -98.1958, -1116.53)
+    camera.SetViewUp(-0.00203823, -0.0605367, 0.998164)
+    camera.SetFocalPoint(19, 91, -1105)
+    camera.SetViewAngle(30) 
+    cameraNode.ResetClippingRange() 
+    self.threeDView.setDisabled(True)
 
